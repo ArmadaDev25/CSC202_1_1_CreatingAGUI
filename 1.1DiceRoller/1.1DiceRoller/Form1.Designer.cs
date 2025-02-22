ffe701ca0ea66252b46c280cbbc6cf184360c11d
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2025-02-22 15:12:04 -0700

    Added Some UI Elements to the designer

--- 1.1DiceRoller/1.1DiceRoller/Form1.Designer.cs
+++ 1.1DiceRoller/1.1DiceRoller/Form1.Designer.cs
@@ -28,12 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(25, 205);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 0;
+            button1.Text = "Roll Dice";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(25, 45);
+            label1.Name = "label1";
+            label1.Size = new Size(195, 25);
+            label1.TabIndex = 1;
+            label1.Text = "Number of Dice To Roll";
+            label1.Click += label1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Dice Roller";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Label label1;
     }
 }
--- 1.1DiceRoller/1.1DiceRoller/obj/Debug/net8.0-windows/1.1DiceRoller.AssemblyInfo.cs
+++ 1.1DiceRoller/1.1DiceRoller/obj/Debug/net8.0-windows/1.1DiceRoller.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1.1DiceRoller")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49b5894ddd05e77d81da0f3e122b14088150d453")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0712ddc5fac7fe2004a03c0a9cbcdc255e871e26")]
 [assembly: System.Reflection.AssemblyProductAttribute("1.1DiceRoller")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1.1DiceRoller")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
